45ccb41a8c8d7092a7da841176f64d45fb5cf263
Simkin Mark <trial_sim@mail.ru>
2020-05-10 08:32:51 +0400

    add grid data

--- src/renderer/components/manager/ManagerGrid.vue
+++ src/renderer/components/manager/ManagerGrid.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="manager-grid">
+  <div class="manager-grid" :style="style">
     <ManagerItem
       v-for="(item, i) in items"
       :key="item.id"
@@ -22,6 +22,20 @@ export default {
       required: true,
       type: Array
     }
+  },
+
+  data: () => ({
+    columns: 5,
+    rows: 5
+  }),
+
+  computed: {
+    style () {
+      return {
+        'grid-template-columns': `repeat(${this.columns}, 1fr)`,
+        'grid-template-rows': `repeat(${this.rows}, 1fr)`
+      }
+    }
   }
 }
 </script>
@@ -31,8 +45,6 @@ export default {
   display: grid;
   flex: 1;
   grid-gap: 10px;
-  grid-template-columns: repeat(5, 1fr);
-  grid-template-rows: repeat(5, 1fr);
   margin-bottom: 10px;
   padding: 0 20px;
 }
